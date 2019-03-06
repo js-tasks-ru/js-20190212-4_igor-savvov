1d9dea1004d6ac955c96e66da3200000f0190701
Igor <380931547944@ex.ua>
2019-03-06 21:21:13 +0200

    Finished function clone()

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,9 @@
  * @returns {Object}
  */
 function clone (obj) {
+  let copyObj = {};
+  for (const key in obj) {
+    typeof obj[key] === 'object' && obj[key] != null ? copyObj[key] = clone(obj[key]) : copyObj[key] = obj[key];
+  }
+  return copyObj;
 }
