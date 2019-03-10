8b3fa3536446569daca00fe9a1065d3929c473a1
Igor <380931547944@ex.ua>
2019-03-10 20:56:49 +0200

    Finished function makeFriendsList()

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+  if (!friends.length) return;
+  let ul = document.createElement('ul');
+  friends.forEach((el) => {
+    let li = document.createElement('li');
+    li.innerHTML = `${el.firstName} ${el.lastName}`;
+    ul.appendChild(li);
+  });
+  return ul;
 }
