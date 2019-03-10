5688221beabdab3842a9fea0e2dafd2f686b9d5d
Igor <380931547944@ex.ua>
2019-03-10 20:56:07 +0200

    Finished calendar iterator

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,29 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+
+  let current = this.from,
+      last = this.to;
+
+  function isWeekend(day) {
+    return day.getDay() === 0 || day.getDay() === 6 ? true : false;
+  }
+  return {
+    next() {
+      if (current <= last) {
+        current.setDate(current.getDate() + 1);
+        let date = `0${current.getDate()}`.slice(-2);
+        return {
+          done: false,
+          value: isWeekend(current) ? `[${date}]` : date
+        };
+      } else {
+        return {
+          done: true
+        };
+      }
+    }
+  }
 };
 
 
