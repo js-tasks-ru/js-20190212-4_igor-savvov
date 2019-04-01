15c419548907cd582f42a29cf54a5f91f8a1f3a8
Igor <380931547944@ex.ua>
2019-04-01 23:32:36 +0300

    Finished function extendClass()

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -6,6 +6,6 @@
  * @param {Function} Parent - родительский класс
  */
 function extendClass(Main, Parent) {
-    // здесь ваш код наследования
+  Main.prototype = Object.create(Parent.prototype);
 }
 
