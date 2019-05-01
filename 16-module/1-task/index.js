3de8b4fe5d5701a27bb6c92c6743333945075d10
Igor <380931547944@ex.ua>
2019-05-01 15:06:02 +0300

    Finished function processGenerator()

--- 16-module/1-task/index.js
+++ 16-module/1-task/index.js
@@ -3,7 +3,17 @@
  * @param {Generator} gen - генератор
  * @returns {Promise}
  */
-function processGenerator(gen) {
+function processGenerator(gen, yieldVal) {
+  let next = gen.next(yieldVal);
+
+  if (!next.done) {
+    return next.value.then(
+      result => processGenerator(gen, result),
+      err => gen.throw(err)
+    );
+  } else {
+    return new Promise(resolve => resolve(next.value));
+  }
 }
 
 
